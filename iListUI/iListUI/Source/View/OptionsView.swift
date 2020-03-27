56439b32a513bb6c17e54fe17f984fd0569ed7dd
Simon <simon@aparicio.org>
2020-03-27 05:19:25 +0100

    created the sorting type

--- iListUI/iListUI/Source/View/OptionsView.swift
+++ iListUI/iListUI/Source/View/OptionsView.swift
@@ -10,46 +10,92 @@ import SwiftUI
 struct OptionsView: View {
 
     @Environment(\.presentationMode) var presentationMode
+    
+    // set default values
+    @State private var selectedSorting = SortingType.alphabetical
+    @State private var selectedSortingOption = SortingOptionType.descending
+    @State private var descOrder = true
+    @State private var showWatchedOnly = false
+    @State private var showFavouriteOnly = false
+    @State private var showFeaturedOnly = false
+    @State private var maxPopularity = 5 {
+        didSet { // limit the max and min value that the user can selected for popularity filter
+            if maxPopularity > 5 { maxPopularity = 5 }
+            if maxPopularity < 1 { maxPopularity = 1 }
+        }
+    }
 
     let optionsTitle = "Opciones"
 
     var body: some View {
         
         NavigationView {
-            
             Form {
+                Section(header: Text("Selecciona los criterios de filtrado")
+                                    .font(.system(.body, design: .rounded))
+                                    .fontWeight(.regular)) {
+                    Toggle(isOn: $showWatchedOnly){
+                        Text("Mostrar solo cursos vistos")
+                    }
+                    Toggle(isOn: $showFavouriteOnly){
+                        Text("Mostrar solo cursos favoritos")
+                    }
+                    Toggle(isOn: $showFeaturedOnly){
+                        Text("Mostrar solo cursos destacados")
+                    }
+
+                    Stepper(onIncrement: {
+                        self.maxPopularity += 1
+                    }, onDecrement: {
+                        self.maxPopularity -= 1
+                    }){
+                        HStack {
+                            Text("Mostrar")
+                            Text("\(String(repeating: AppConfig.popularityChar, count: maxPopularity))")
+                                .font(.headline)
+                                .foregroundColor(.secondary)
+                            Text("o menos")
+                        }
+                    }
+                }
+
+                Section(header: Text("Selectiona los criterios de ordenación")
+                                    .font(.system(.body, design: .rounded))
+                                    .fontWeight(.regular)) {
+                    Picker(selection: $selectedSorting, label: Text("Ordenar")){
+                        ForEach(SortingType.allCases, id: \.self){ sortingType in
+                            Text(sortingType.description)
+                        }
+                    }
+                    Picker(selection: $selectedSortingOption, label: Text("Mostrar")){
+                        ForEach(SortingOptionType.allCases, id: \.self){ sortingOptionType in
+                            Text(sortingOptionType.description)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
                 
-                Section(header: Text("Criterios de ordenación")){
-                    Text("Aquí las opciones de ordenación")
-                }
-
-                Section(header: Text("Criterios de filtrado")){
-                    Text("Aquí las opciones de filtrado")
-                }
-
-            }
-        .navigationBarTitle(optionsTitle)
-        //.navigationBarTitle(Text(optionsTitle), displayMode: .inline)
-
-        .navigationBarItems(
-            leading: Button(action: { // left button
-                self.presentationMode.wrappedValue.dismiss()
-            }, label: {
-                Image(systemName:"rectangle.dock")
-                    .font(.title)
-            }),
-            trailing: Button(action: { // right button
-
+            }//form
+            .navigationBarTitle(optionsTitle)
+            //.navigationBarTitle(Text(optionsTitle), displayMode: .inline)
+                
+            .navigationBarItems(
+                leading: Button(action: { // left button
+                    self.presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Image(systemName:"rectangle.dock")
+                        .font(.title)
+                }),
+                trailing: Button(action: { // right button
+                    
                 // TODO: Save values, update EnvironmentObject
                 self.presentationMode.wrappedValue.dismiss() // close view
-                
-            }, label: {
-                Image(systemName: "rectangle.fill.badge.checkmark")
-                    .font(Font(UIFont.AppFont.compactTitle))
-            })
+                }, label: {
+                    Image(systemName: "rectangle.fill.badge.checkmark")
+                        .font(Font(UIFont.AppFont.compactTitle))
+                })
             )
-            
-        }
+        
+        }//navigation view
     }
 }
 
